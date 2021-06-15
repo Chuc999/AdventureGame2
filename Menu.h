4fdc5c7c3cedd000412fd4eec4584895a901ac53
Chuc <chuc_is_da1@hotmail.com>
2021-06-15 15:53:27 +1000

    Added an empty class

---
+++ Menu.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+using namespace std;
+class Menu
+{
+protected:
+	struct menuOptions
+	{
+		string one = " New Game";
+		string two = " Load Game";
+		string three = " Quit";
+	};
+
+
+};
+
